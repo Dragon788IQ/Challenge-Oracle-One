c0100678371839903da89e015e5557558a387d55
Johann Aguayo <johann200_cardenas788@hotmail.com>
2023-01-14 17:29:35 -0600

    agregando js pero rompiendo el css

---
+++ estilos/componente.js
@@ -0,0 +1,8 @@
+function desplegar_1(){
+    var x = document.getElementById("encriptado")
+    if (x.style.display === 'none') {
+        x.style.display = 'inline-block';
+      } else {
+        x.style.display = 'none';
+      }
+}
