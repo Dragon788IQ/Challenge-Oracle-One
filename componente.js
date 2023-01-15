5f20eb1f90ec61db78cb31faad573dd781359a4a
Johann Aguayo <johann200_cardenas788@hotmail.com>
2023-01-14 20:37:26 -0600

    Mejorando la logica del js

--- componente.js
+++ componente.js
@@ -1,22 +1,39 @@
 
 function deplegar_1() {
-  var x = document.getElementById('encriptado');
-  var y = document.getElementById('desencriptado');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-    y.style.display = 'none';
+  let contenedor_1 = document.getElementById('encriptado');
+  let contenedor_2 = document.getElementById('desencriptado');
+  let texto_1 = document.getElementById("texto_1")
+  let texto_2 = document.getElementById("texto_2")
+
+  if (contenedor_1.style.display === 'none') {
+    contenedor_1.style.display = 'block';
+    contenedor_2.style.display = 'none';
   } else {
-    x.style.display = 'none';
+    contenedor_1.style.display = 'none';
   }
+  codificador(texto_1)
 }
 
 function deplegar_2() {
-  var x = document.getElementById('desencriptado');
-  var y = document.getElementById('encriptado');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-    y.style.display = 'none';
+  let contenedor_2 = document.getElementById('desencriptado');
+  let contenedor_1 = document.getElementById('encriptado');
+  let texto_1 = document.getElementById("texto_1")
+  let texto_2 = document.getElementById("texto_2")
+
+  if (contenedor_2.style.display === 'none') {
+    contenedor_2.style.display = 'block';
+    contenedor_1.style.display = 'none';
+    codificador(texto_2)
   } else {
-    x.style.display = 'none';
+    contenedor_2.style.display = 'none';
   }
+}
+
+function codificador(texto){
+  let mensaje_raw = document.getElementById('mensaje').value.toString();
+  let mensaje_process = [];
+  var contador =0;
+  console.log(typeof mensaje_raw);
+  mensaje_raw=mensaje_raw.replace("o", "ine")
+  texto_1.textContent = mensaje_raw;
 }
